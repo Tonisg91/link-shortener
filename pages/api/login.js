e0b04dde7fdc889aae155b87a134a97f3fb76135
Tonisg91 <sancheztoni.garcia@gmail.com>
2021-10-22 08:18:21 +0200

    login api

--- pages/api/login.js
+++ pages/api/login.js
@@ -2,12 +2,22 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function Login(req, res) {
-  console.log(req.body)
   try {
+    const hasUser = await prisma.user.findFirst({
+      where: { firebaseId: req.body.firebaseId }
+    })
+
+    if (hasUser) {
+      return res.status(200).json({ data: hasUser })
+    }
+
     const user = await prisma.user.create({
       data: { ...req.body }
     })
 
-    console.log(user)
-  } catch (error) {}
+    return res.status(200).json({ data: user })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: error.message })
+  }
 }
